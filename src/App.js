90e1c532c61fbc8caf5ebfef6ba9760949c56805
InesGTavares <tavares.inesg@gmail.com>
2021-02-12 23:12:09 +0000

    Added GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; 
+import "./App.css"; 
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+      Weather Application
+        <footer>This project was coded by Inês Tavares and is open-source on <a href="https://github.com/InesGTavares/react-project-app"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+        >GitHub</a>
+        </footer>
           Learn React
-        </a>
-      </header>
     </div>
   );
 }
